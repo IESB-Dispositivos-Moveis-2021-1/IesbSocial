00726def7cfd75f3f7227a4c4c1cc3701d7549b4
Pedro Henrique <intellitour@gmail.com>
2021-09-02 19:39:05 -0300

    Initial Commit

---
+++ IesbSocial/IesbSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  IesbSocialApp.swift
+//  IesbSocial
+//
+//  Created by Pedro Henrique on 02/09/21.
+//
+
+import SwiftUI
+
+@main
+struct IesbSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
